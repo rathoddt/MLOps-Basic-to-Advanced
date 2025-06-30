5078b870a32bf11cc37e5d9ab4ef1839379381f3
Dilip Rathod <rathod.dt@gmail.com>
2025-06-30 06:22:46 +0000

    Data preprocessing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import numpy as np
+from src.logger import get_logger
+from src.custom_exception import CustomException
+from config.paths_config import *
+from utils.common_functions import read_yaml,load_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import SMOTE
+
+logger = get_logger(__name__)
+
+class DataProcessor:
+
+    def __init__(self, train_path, test_path, processed_dir, config_path):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.processed_dir = processed_dir
+
+        self.config = read_yaml(config_path)
+
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+
+    def preprocess_data(self,df):
+        try:
+            logger.info("Starting our Data Processing step")
+
+            logger.info("Dropping the columns")
+            df.drop(columns=['Unnamed: 0', 'Booking_ID'] , inplace=True)
+            df.drop_duplicates(inplace=True)
+
+            cat_cols = self.config["data_preprocessing"]["categorical_cols"]
+            num_cols = self.config["data_preprocessing"]["numerical_cols"]
+                  
